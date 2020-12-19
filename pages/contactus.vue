9a564adf5f9aec4ffcab353e27336dc04b5c4db4
wael salah <waelsalah@localhost.localdomain>
2020-12-19 12:12:23 +0200

    contact form

--- pages/contactus.vue
+++ pages/contactus.vue
@@ -7,40 +7,32 @@
         </div>
       </div>
       <div class="row">
-        <div class="col-sm-8">
+        <div class="col-sm-12">
           <div class="contact-form">
-            <h2 class="title text-center">Get In Touch</h2>
-            <div class="status alert alert-success" style="display: none"></div>
-            <form id="main-contact-form" class="contact-form row" name="contact-form" method="post">
+            <div class="status alert alert-success" v-if="this.success_message !== ''">{{ this.success_message }}</div>
+            <div v-if="this.errors.length > 0" class="alert alert-danger">
+              <p>Please fix these errors</p><br/>
+              <ul style="padding: 0">
+                <li v-for="error in this.errors" :key="error">{{ error }}</li>
+              </ul>
+            </div>
+            <form id="main-contact-form" class="contact-form row" name="contact-form" method="post" @submit.prevent="submit()">
               <div class="form-group col-md-6">
-                <input type="text" name="name" class="form-control" required="required" placeholder="Name">
+                <input type="text" name="name" class="form-control"  placeholder="Name" v-model="name">
               </div>
               <div class="form-group col-md-6">
-                <input type="email" name="email" class="form-control" required="required" placeholder="Email">
+                <input type="email" name="email" class="form-control" placeholder="Email" v-model="email">
               </div>
               <div class="form-group col-md-12">
-                <input type="text" name="subject" class="form-control" required="required" placeholder="Subject">
+                <input type="text" name="subject" class="form-control"  placeholder="Subject" v-model="subject">
               </div>
               <div class="form-group col-md-12">
-                <textarea name="message" id="message" required="required" class="form-control" rows="8" placeholder="Your Message Here"></textarea>
+                <textarea name="message" id="message"  class="form-control" rows="8" placeholder="Your Message Here" v-model="message"></textarea>
               </div>
               <div class="form-group col-md-12">
-                <input type="submit" name="submit" class="btn btn-primary pull-right" value="Submit">
+                <input type="submit" name="submit" class="btn btn-primary pull-right" value="Submit" >
               </div>
             </form>
-          </div>
-        </div>
-        <div class="col-sm-4">
-          <div class="contact-info">
-            <h2 class="title text-center">Contact Info</h2>
-            <address>
-              <p>E-Shopper Inc.</p>
-              <p>935 W. Webster Ave New Streets Chicago, IL 60614, NY</p>
-              <p>Newyork USA</p>
-              <p>Mobile: +2346 17 38 93</p>
-              <p>Fax: 1-714-252-0026</p>
-              <p>Email: info@e-shopper.com</p>
-            </address>
           </div>
         </div>
       </div>
@@ -51,21 +43,73 @@
 <script>
     export default {
         name: "Contactus",
-      head() {
-        return {
-          title: 'Online Shop | Contact us',
-          meta: [
-            {
-              hid: 'description',
-              name: 'description',
-              content: 'Contact us Page'
+        data() {
+          return {
+            name: "",
+            email: "",
+            subject: "",
+            message: "",
+            errors: [],
+            success_message: ""
+          }
+        },
+        head() {
+          return {
+            title: 'Online Shop | Contact us',
+            meta: [
+              {
+                hid: 'description',
+                name: 'description',
+                content: 'Contact us Page'
+              }
+            ]
+          }
+        },
+        methods: {
+          submit() {
+            const errors = [];
+            if(this.name === "") {
+              errors.push("name required");
             }
-          ]
+
+            if(this.email === "") {
+              errors.push("email required");
+            }
+
+            if(this.subject === "") {
+              errors.push("subject required");
+            }
+
+            if(this.message === "") {
+              errors.push("message contents required");
+            }
+
+            if(errors.length > 0) {
+              this.errors = errors;
+              return false;
+            }
+
+            this.errors = [];
+
+            const data = {
+              name: this.name,
+              email: this.email,
+              subject: this.subject,
+              message: this.message
+            };
+
+            this.$axios.post("/api/contact", data).then(res => {
+
+              this.success_message = res.data.message;
+
+              this.name = "";
+              this.email = "";
+              this.subject = "";
+              this.message = "";
+            }).catch(err => {
+              console.error(err);
+            });
+          }
         }
-      }
     }
 </script>
-
-<style scoped>
-
-</style>
